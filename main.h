9c1edefebbf2f89e4711048fd0a1a6b010458e2a
Olivia Madson <124902143+hasholivia@users.noreply.github.com>
2023-02-09 14:03:00 -0300

    main library

---
+++ main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+//#include "db.h"
+#include "handle.h"
+#include "functions.h"
+#include "template_functions.h"
+#include "global.h"
+
+//#define DEBUG TRUE
+#define TRUE 1
+#define FALSE 0
+//#define MAX_WORDS 40
+//#define MAX_LETTERS 20
+#define MAX_WORDS 80
+#define MAX_LETTERS 80
+
+#endif // MAIN_H_INCLUDED
